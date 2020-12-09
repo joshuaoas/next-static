1a101f244348fcd58598f85383ea32fcdbb9b30b
Josh <joas@spacesaver.com>
2020-12-08 22:32:15 -0600

    added login and signup views and cleaned up link components

--- components/Header.js
+++ components/Header.js
@@ -11,10 +11,12 @@ export default function Header(props) {
   <div className="relative z-20 shadow">
     <div className="max-w-7xl mx-auto flex justify-between items-center px-4 py-5 sm:px-6 sm:py-4 lg:px-8 md:justify-start md:space-x-10">
       <div>
-        <a href="#" className="flex">
+      <Link href="/">
+        <a className="flex">
           <span className="sr-only">Workflow</span>
           <img className="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="" />
         </a>
+        </Link>
       </div>
       <div className="-mr-2 -my-2 md:hidden">
         <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -40,12 +42,14 @@ export default function Header(props) {
 
         </nav>
         <div className="flex items-center md:ml-12">
-          <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
-            Sign in
-          </a>
-          <a href="#" className="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+            <Link href="/login" >
+            <a className="text-base font-medium text-gray-500 hover:text-gray-900">Sign in</a>
+            </Link>
+            <Link href="/signup" >
+          <a className="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
             Sign up
           </a>
+          </Link>
         </div>
       </div>
     </div>
